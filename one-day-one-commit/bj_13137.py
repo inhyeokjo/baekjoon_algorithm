1516182e9020123f410e301329505560eb8490e2
조인혁 <alzpqm@naver.com>
2022-02-08 17:04:54 +0900

    feat: 동적계획법을 직접 실행해서 그 결과가 그리디법이랑 동일한지 비교하는 방식으로 해결

--- one-day-one-commit/bj_13137.py
+++ one-day-one-commit/bj_13137.py
@@ -1,14 +1,31 @@
 import sys
+
 input = sys.stdin.readline
 
 max = int(input())
 monetary_units = list(map(int, input().strip().split()))
+monetary_units.reverse()
+
+cache = [1 if i in monetary_units else 99999 for i in range(monetary_units[0] * 2)]
+cache[0] = 0
+
 no = False
 
-for i in range(len(monetary_units)-1):
-    if monetary_units[i+1]%monetary_units[i] != 0:
-        print('No')
-        no=True
+for i in range(2, monetary_units[0] * 2):
+    first = True
+    for j in range(max):
+        if i == monetary_units[j]:
+            break
+        if i > monetary_units[j]:
+            if first:
+                cache[i] = cache[i - monetary_units[j]] + 1
+                first = False
+                continue
+            if cache[i - monetary_units[j]] + 1 < cache[i]:
+                print('No')
+                no = True
+                break
+    if no:
         break
 if not no:
     print('Yes')
